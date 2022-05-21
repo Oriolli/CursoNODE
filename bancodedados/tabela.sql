73d576c025f13f7fb362abc44953cc73b763bf5e
oriolli <mar.orioli@hotmail.com>
2022-05-20 21:16:30 -0300

    Mais de SQL

--- bancodedados/tabela.sql
+++ bancodedados/tabela.sql
@@ -1,4 +1,26 @@
+/* comando para criar tabelas
 create table usuarios(
   nome varchar(50),
   email varchar(100),
-  idade int);
+  idade int);   */
+
+  /* comando para inserir dados em uma tabela
+  INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Julia Souza Orioli",
+    "julia.souza@gmail.com",
+    11
+  );
+   */
+   /* comando para selecionar um parâmetro específico
+   SELECT * FROM usuarios WHERE idade = 8;
+   */
+
+   /* comando para selecionar apenas usuários com um nome espacífico
+    SELECT * FROM usuarios WHERE nome = "Julia Souza Orioli";
+    */
+
+    /* comando para selecionar com condições
+    SELECT * FROM usuarios WHERE idade >= 11;
+    */
+
+    
